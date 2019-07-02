6c4949db2c617bdca5ced85a0184a8b6ccb81a48
josh <joshborup@gmail.com>
2019-07-02 16:15:19 -0700

    Updates

---
+++ precache-manifest.d828a56c957287bb5019c4fc0a7fb71f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b80e9028e1cfe4a5b38e040b33eb3ef0",
+    "url": "/joshborup/portfolio-josh/index.html"
+  },
+  {
+    "revision": "3c6fef6661ba2f735eae",
+    "url": "/joshborup/portfolio-josh/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/joshborup/portfolio-josh/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "3c6fef6661ba2f735eae",
+    "url": "/joshborup/portfolio-josh/static/js/main.71e5ff53.chunk.js"
+  },
+  {
+    "revision": "70878b062a0de4c2eb93",
+    "url": "/joshborup/portfolio-josh/static/js/runtime~main.e47706f4.js"
+  }
+]);
